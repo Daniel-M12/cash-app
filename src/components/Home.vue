05f2fe6aa2b0aa64ca9afa5d3ee9e5b3f69f38e2
Daniel Matos <deme12.1@gmail.com>
2024-12-30 09:36:55 -0500

    Create home, layout and header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-
 <template>
-  <h1>Home</h1>
+  <Layout>
+    <template #header>
+      <Header></Header>
+    </template>
+  </Layout>
 </template>
+
+<script setup lang="ts">
+import Layout from './Layout.vue'
+import Header from './Header.vue'
+</script>
