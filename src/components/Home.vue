6c5342691d9db51028f5c2fc56a24813738dcce7
Daniel Matos <deme12.1@gmail.com>
2024-12-30 10:31:17 -0500

    Add Resume and Movements sections to Layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,10 +3,18 @@
     <template #header>
       <Header></Header>
     </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
   </Layout>
 </template>
 
 <script setup lang="ts">
 import Layout from './Layout.vue'
 import Header from './Header.vue'
+import Resume from './resume/Index.vue'
+import Movements from './Movements.vue'
 </script>
