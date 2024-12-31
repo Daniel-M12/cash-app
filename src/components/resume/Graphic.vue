c9c62fe069b7a076c04f1ca7842db014bce66544
Daniel Matos <deme12.1@gmail.com>
2024-12-31 14:22:59 -0500

    Implements touch cursor in graph

--- src/components/resume/Graphic.vue
+++ src/components/resume/Graphic.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
-    <svg viewBox="0 0 300 200">
+    <svg @touchstart="tap" @touchmove="tap" @touchend="untap" viewBox="0 0 300 200">
       <line stroke="#c4c4c4" stroke-width="2" x1="0" :y1="zero" x2="300" :y2="zero" />
       <polyline fill="none" stroke="#0689B0" stroke-width="2" :points="points" />
-      <line stroke="#04b500" stroke-width="2" x1="200" y1="0" x2="200" y2="200" />
+      <line
+        v-show="showPointer"
+        stroke="#04b500"
+        stroke-width="2"
+        :x1="pointer"
+        y1="0"
+        :x2="pointer"
+        y2="200"
+      />
     </svg>
     <p>Últimos 30 días</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps, toRefs } from 'vue'
+import { computed, defineProps, ref, toRefs } from 'vue'
 
 const props = defineProps({
   amounts: {
@@ -45,6 +53,25 @@ const points = computed(() => {
     return `${points} ${x},${y}`
   }, '0,100')
 })
+
+const showPointer = ref(false)
+const pointer = ref(0)
+
+const tap = (event: TouchEvent) => {
+  const { currentTarget, touches } = event
+  showPointer.value = true
+  const elementWidth = (currentTarget as HTMLElement).getBoundingClientRect().width
+  const elementX = (currentTarget as HTMLElement).getBoundingClientRect().x
+  const touchX = touches[0].clientX
+
+  const graphPixels = ((touchX - elementX) * 300) / elementWidth
+
+  pointer.value = graphPixels
+}
+
+const untap = () => {
+  showPointer.value = false
+}
 </script>
 
 <style scoped>
