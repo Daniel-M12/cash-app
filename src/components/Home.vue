cea32772f8a6b61cd08eeb111a7750a133dd91f4
Daniel Matos <deme12.1@gmail.com>
2025-01-01 12:28:40 -0500

    Use localStorage to persist movements

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -27,7 +27,20 @@ import Movements from './movements/Index.vue'
 import type MovementClass from '@/common/model/movement.model'
 import Action from './Action.vue'
 import Graphic from './resume/Graphic.vue'
-import { computed, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
+
+onMounted(() => {
+  const storedMovements = localStorage.getItem('movements')
+  let parsedMovements: MovementClass[] = storedMovements ? JSON.parse(storedMovements) : []
+  if (Array.isArray(parsedMovements)) {
+    movements.value = parsedMovements?.map((m) => {
+      return {
+        ...m,
+        date: new Date(m.date),
+      }
+    })
+  }
+})
 
 const label: string | null = 'Etiqueta'
 const amount: number | null = 1001
@@ -54,6 +67,7 @@ const create = (movement: MovementClass) => {
   console.log('Create movement:', movement)
   console.log('movements before', movements)
   movements.value.push(movement)
+  save()
   console.log('movements after', movements)
 }
 
@@ -61,79 +75,13 @@ const remove = (id: number) => {
   const index = movements.value.findIndex((m) => m.id === id)
   console.log('movements before', movements)
   movements.value.splice(index, 1)
+  save()
   console.log('movements after', movements)
 }
 
-let movements = ref<MovementClass[]>([
-  {
-    id: 1,
-    title: 'Move 1',
-    description: 'Lorem ipsum',
-    amount: 1000,
-    date: new Date('11-30-2024'),
-  },
-  {
-    id: 2,
-    title: 'Move 2',
-    description: 'Lorem ipsum dolor',
-    amount: -100,
-    date: new Date('12-01-2024'),
-  },
-  {
-    id: 3,
-    title: 'Move 3',
-    description: 'Lorem ipsum dolor',
-    amount: 300,
-    date: new Date('12-06-2024'),
-  },
-  {
-    id: 4,
-    title: 'Move 4',
-    description: 'Lorem ipsum',
-    amount: 1000,
-    date: new Date('12-07-2024'),
-  },
-  {
-    id: 5,
-    title: 'Move 5',
-    description: 'Lorem ipsum dolor',
-    amount: -100,
-    date: new Date('12-07-2024'),
-  },
-  {
-    id: 6,
-    title: 'Move 6',
-    description: 'Lorem ipsum dolor',
-    amount: 300,
-    date: new Date('12-07-2024'),
-  },
-  {
-    id: 7,
-    title: 'Move 7',
-    description: 'Lorem ipsum',
-    amount: -1000,
-    date: new Date('12-08-2024'),
-  },
-  {
-    id: 8,
-    title: 'Move 8',
-    description: 'Lorem ipsum dolor',
-    amount: -100,
-    date: new Date('12-08-2024'),
-  },
-  {
-    id: 9,
-    title: 'Move 9',
-    description: 'Lorem ipsum dolor',
-    amount: 300,
-    date: new Date('12-09-2024'),
-  },
-  {
-    id: 10,
-    title: 'Move 10',
-    description: 'Lorem ipsum dolor',
-    amount: 300,
-    date: new Date('12-09-2024'),
-  },
-])
+const save = () => {
+  localStorage.setItem('movements', JSON.stringify(movements.value))
+}
+
+let movements = ref<MovementClass[]>([])
 </script>
