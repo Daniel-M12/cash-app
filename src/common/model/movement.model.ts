cec4e623afda46f296ee3d6bd7a2c86d9076a53e
Daniel Matos <deme12.1@gmail.com>
2024-12-31 20:15:43 -0500

    Use dinamic amounts with last 30 days to create the graphic

--- src/common/model/movement.model.ts
+++ src/common/model/movement.model.ts
@@ -3,6 +3,7 @@ interface MovementType {
   title: string
   description: string
   amount: number
+  date: Date
 }
 
 export default class MovementClass implements MovementType {
@@ -10,11 +11,13 @@ export default class MovementClass implements MovementType {
   title: string
   description: string
   amount: number
+  date: Date
 
-  constructor(id: number, title: string, description: string, amount: number) {
+  constructor(id: number, title: string, description: string, amount: number, date: Date) {
     this.id = id
     this.title = title
     this.description = description
     this.amount = amount
+    this.date = date
   }
 }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
     <template #resume>
       <Resume :total-label="'Ahorro total'" :label="label" :total="100000" :amount="amount">
         <template #graphic>
-          <Graphic :amounts="[100, 200, 500, 200, -400, -600, -300, 0, 300, 500]" />
+          <Graphic :amounts="amounts" />
         </template>
         <template #action>
           <Action />
@@ -27,9 +27,29 @@ import Movements from './movements/Index.vue'
 import type MovementClass from '@/common/model/movement.model'
 import Action from './Action.vue'
 import Graphic from './resume/Graphic.vue'
+import { computed } from 'vue'
 
 const label: string | null = 'Etiqueta'
 const amount: number | null = 1001
+
+const amounts = computed(() => {
+  const lastDaysMovements = movements
+    .filter((m: MovementClass) => {
+      const today: Date = new Date()
+      console.log('today:', today)
+      const oldDate: Date = new Date(today.setDate(today.getDate() - 30)) // Create a new Date object
+      return m.date > oldDate
+    })
+    .map((m: MovementClass) => m.amount)
+
+  return lastDaysMovements.map((m, i) => {
+    const lastMovements = lastDaysMovements.slice(0, i)
+
+    return lastMovements.reduce((sum, movement) => {
+      return sum + movement
+    }, 0)
+  })
+})
 
 const movements: MovementClass[] = [
   {
@@ -37,18 +57,70 @@ const movements: MovementClass[] = [
     title: 'Move 1',
     description: 'Lorem ipsum',
     amount: 1000,
+    date: new Date('11-30-2024'),
   },
   {
     id: 2,
     title: 'Move 2',
     description: 'Lorem ipsum dolor',
     amount: -100,
+    date: new Date('12-01-2024'),
   },
   {
     id: 3,
     title: 'Move 3',
     description: 'Lorem ipsum dolor',
     amount: 300,
+    date: new Date('12-06-2024'),
+  },
+  {
+    id: 4,
+    title: 'Move 4',
+    description: 'Lorem ipsum',
+    amount: 1000,
+    date: new Date('12-07-2024'),
+  },
+  {
+    id: 5,
+    title: 'Move 5',
+    description: 'Lorem ipsum dolor',
+    amount: -100,
+    date: new Date('12-07-2024'),
+  },
+  {
+    id: 6,
+    title: 'Move 6',
+    description: 'Lorem ipsum dolor',
+    amount: 300,
+    date: new Date('12-07-2024'),
+  },
+  {
+    id: 7,
+    title: 'Move 7',
+    description: 'Lorem ipsum',
+    amount: -1000,
+    date: new Date('12-08-2024'),
+  },
+  {
+    id: 8,
+    title: 'Move 8',
+    description: 'Lorem ipsum dolor',
+    amount: -100,
+    date: new Date('12-08-2024'),
+  },
+  {
+    id: 9,
+    title: 'Move 9',
+    description: 'Lorem ipsum dolor',
+    amount: 300,
+    date: new Date('12-09-2024'),
+  },
+  {
+    id: 10,
+    title: 'Move 10',
+    description: 'Lorem ipsum dolor',
+    amount: 300,
+    date: new Date('12-09-2024'),
   },
 ]
 </script>
